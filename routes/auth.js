ec17aeca7629306440649a07a1eda8ce71652987
Bhola nath Sarkar <sarkaraditya817@gmail.com>
2024-08-12 09:18:45 +0530

    Modify end points

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 
 const authController = require("../controllers/auth");
 
-router.post("/api/login", authController.login);
+router.post("/login", authController.login);
 
-router.post("/api/register", authController.register, authController.sendOTP);
-router.post("/api/verify", authController.verifyOTP);
-router.post("/api/send-otp", authController.sendOTP);
+router.post("/register", authController.register, authController.sendOTP);
+router.post("/verify", authController.verifyOTP);
+router.post("/send-otp", authController.sendOTP);
 
-router.post("/api/forgot-password", authController.forgotPassword);
-router.post("/api/reset-password", authController.resetPassword);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,16 @@ const router = require("express").Router();
 const userController = require("../controllers/user");
 const authController = require("../controllers/auth");
 
-router.patch("/api/update-me", authController.protect, userController.updateMe);
+router.patch("/update-me", authController.protect, userController.updateMe);
 
-router.get("/api/get-users", authController.protect, userController.getUsers);
+router.get("/get-users", authController.protect, userController.getUsers);
 router.get(
-  "/api/get-friends",
+  "/get-friends",
   authController.protect,
   userController.getFriends
 );
 router.get(
-  "/api/get-friends-request",
+  "/get-friends-request",
   authController.protect,
   userController.getRequests
 );
